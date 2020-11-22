65a634a28113cc690dd29799d9f21712f05ae84a
racheldunne-CO <racheldunne@cabinetoffice.gov.uk>
2020-11-22 16:05:10 +0000

    Lint resistance.py

--- resistance.py
+++ resistance.py
@@ -8,7 +8,7 @@ Created on Sat Nov 14 15:05:23 2020
 import random
 import os
 
-    
+
 def input_players():
     number = int(input("How many players are there? "))
     if type(number) is not int:
@@ -16,9 +16,9 @@ def input_players():
         number = int(input("How many players are there? "))
     players = []
     for i in range(number):
-        players.append(input("Player" + str(i+1) +": ").lower())
-    return(players)
-    
+        players.append(input("Player" + str(i + 1) + ": ").lower())
+    return players
+
 
 def determine_roles_and_rounds(players):
     """ Assigns players as either resistance  or spies """
@@ -26,27 +26,27 @@ def determine_roles_and_rounds(players):
     if number == 5:
         resistance = 3
         spies = 2
-        rounds = [2,3,2,3,3]
+        rounds = [2, 3, 2, 3, 3]
     elif number == 6:
         resistance = 4
         spies = 2
-        rounds = [2,3,4,3,4]
+        rounds = [2, 3, 4, 3, 4]
     elif number == 7:
         resistance = 4
         spies = 3
-        rounds = [2,3,3,4,4]
+        rounds = [2, 3, 3, 4, 4]
     elif number == 8:
         resistance = 5
         spies = 3
-        rounds = [3,4,4,5,5]
+        rounds = [3, 4, 4, 5, 5]
     elif number == 9:
         resistance = 6
         spies = 3
-        rounds = [3,4,4,5,5]
+        rounds = [3, 4, 4, 5, 5]
     elif number == 10:
         resistance = 6
         spies = 4
-        rounds = [3,4,4,5,5]
+        rounds = [3, 4, 4, 5, 5]
     else:
         print("Invalid player number")
     spy_players = []
@@ -59,26 +59,28 @@ def determine_roles_and_rounds(players):
         resistance_person = random.choice(players)
         resistance_players.append(resistance_person)
         players.remove(resistance_person)
-    setup = {"spies" : spy_players, "resistance" : resistance_players,
-             "rounds": rounds}
-    return(setup)
-    
+    setup = {"spies": spy_players, "resistance": resistance_players, "rounds": rounds}
+    return setup
+
 
 def choose_participants(number, players):
     print("This round requires ", number, " cards")
     participants = []
     for i in range(number):
-        participants.append(input(f"Who will play this round? (Player {i+1}): ").lower())
+        participants.append(
+            input(f"Who will play this round? (Player {i+1}): ").lower()
+        )
     return participants
-    
+
 
 def get_entries(participants):
-    entries = {k:None for k in participants}
+    entries = {k: None for k in participants}
     for person in participants:
         while entries[person] not in ["bad", "good"]:
             entries[person] = input(f"{person}, what is your choice? (bad/good) ")
             os.system("clear")
     return entries
+
 
 def determine_round(entries, outcomes):
     if "bad" in entries.values():
@@ -96,16 +98,17 @@ def play_resistance():
     players = input_players()
     print(players)
     setup = determine_roles_and_rounds(players)
-    print("This game has five rounds. A round fails if there is one bad card in the hand.")
+    print(
+        "This game has five rounds. A round fails if there is one bad card in the hand."
+    )
     print("The number of cards in each round are: ")
     for i in range(5):
-        print("Round", i+1, "=", setup["rounds"][i], "cards")
+        print("Round", i + 1, "=", setup["rounds"][i], "cards")
     outcomes = []
     for i in range(5):
-        print("Round", i+1, ":")
+        print("Round", i + 1, ":")
         participants = choose_participants(setup["rounds"][i], players)
         entries = get_entries(participants)
         outcomes = determine_round(entries, outcomes)
     print(outcomes)
-    return(participants)
-    
+    return participants
