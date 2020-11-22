c0d9a35f0a9c36719dffeec7a2fe1a21deae7664
racheldunne-CO <racheldunne@cabinetoffice.gov.uk>
2020-11-22 16:20:33 +0000

    Make very basic ui pop up

--- resistance_ui.py
+++ resistance_ui.py
@@ -6,3 +6,10 @@ Created on Sun Nov 22 16:06:09 2020
 @author: rachel.dunne
 """
 
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Welcome to Resistance")], [sg.Text("Player 1: "), sg.InputText("")]]
+
+# Create the window
+window = sg.Window("Demo", layout).read()
+
