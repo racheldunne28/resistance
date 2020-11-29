614dbe189dfc1e892bd314633074150b61dc1293
racheldunne-CO <racheldunne@cabinetoffice.gov.uk>
2020-11-29 18:45:30 +0000

    Lint script

--- resistance.py
+++ resistance.py
@@ -9,14 +9,20 @@ import random
 import PySimpleGUI as sg
 
 #### TO DO
-#Sometimes says resistance has won even though a bad card played
-#Add a way of telling people which character they all are and letting the spies know who the others are
-#Allow to go back if input something wrong - maybe an 'are you sure?' message before properly submit.
+# Sometimes says resistance has won even though a bad card played
+# Add a way of telling people which character they all are and letting the spies know who the others are
+# Allow to go back if input something wrong - maybe an 'are you sure?' message before properly submit.
+
 
 def input_player_number():
-    layout = [[sg.Text("Player number:"), sg.InputOptionMenu([5,6,7,8,9,10], 
-                                                             key = "player_number")],
-              [sg.Button("Submit")], [sg.Button("Close")]]
+    layout = [
+        [
+            sg.Text("Player number:"),
+            sg.InputOptionMenu([5, 6, 7, 8, 9, 10], key="player_number"),
+        ],
+        [sg.Button("Submit")],
+        [sg.Button("Close")],
+    ]
     window = sg.Window("Resistance", layout).read()
     event = window[0]
     values = window[1]
@@ -24,19 +30,22 @@ def input_player_number():
         player_number = int(values["player_number"])
     if event == "Close" or event == sg.WIN_CLOSED:
         window.close()
-  #  number = int(input("How many players are there? "))
-  #  if type(number) is not int:
-  #      print("Invalid entry")
-  #      number = int(input("How many players are there? "))
+    #  number = int(input("How many players are there? "))
+    #  if type(number) is not int:
+    #      print("Invalid entry")
+    #      number = int(input("How many players are there? "))
     return player_number
 
- 
+
 def input_players(player_number):
     players = []
     for i in range(player_number):
-        layout = [[sg.Text("Enter player details")], [sg.Text("Player name: "), 
-                                                   sg.InputText("", key = "player")],
-              [sg.Button("Submit")], [sg.Button("Close")]]
+        layout = [
+            [sg.Text("Enter player details")],
+            [sg.Text("Player name: "), sg.InputText("", key="player")],
+            [sg.Button("Submit")],
+            [sg.Button("Close")],
+        ]
         window = sg.Window("Resistance", layout).read()
         event = window[0]
         values = window[1]
@@ -46,7 +55,7 @@ def input_players(player_number):
         if event == "Close" or event == sg.WIN_CLOSED:
             break
             window.close()
-    return(players)
+    return players
 
 
 def determine_roles_and_rounds(players):
@@ -88,40 +97,68 @@ def determine_roles_and_rounds(players):
         resistance_person = random.choice(players)
         resistance_players.append(resistance_person)
         players.remove(resistance_person)
-    players = [player for players in [spy_players, resistance_players] for player in players]
-    setup = {"spies": spy_players, "resistance": resistance_players, "rounds": rounds, "players": players}
+    players = [
+        player for players in [spy_players, resistance_players] for player in players
+    ]
+    setup = {
+        "spies": spy_players,
+        "resistance": resistance_players,
+        "rounds": rounds,
+        "players": players,
+    }
     return setup
 
 
 def choose_participants(number, players):
     print("This round requires ", number, " cards")
-    participants = [] 
+    participants = []
     print(players)
     print(participants)
     for i in range(number):
-        layout = [[sg.Text(f"Player {i+1}:"), sg.Listbox(values=players,default_values=None, size=(10, 10),
-                                                             key = "participant",)],
-              [sg.Button("Submit")], [sg.Button("Close")]]
-        window = sg.Window(title = "Resistance", layout =layout).read()
+        layout = [
+            [
+                sg.Text(f"Player {i+1}:"),
+                sg.Listbox(
+                    values=players,
+                    default_values=None,
+                    size=(10, 10),
+                    key="participant",
+                ),
+            ],
+            [sg.Button("Submit")],
+            [sg.Button("Close")],
+        ]
+        window = sg.Window(title="Resistance", layout=layout).read()
         event = window[0]
         values = window[1]
         if event == "Submit":
             participants.append(values["participant"])
         if event == "Close" or event == sg.WIN_CLOSED:
             window.close()
-    participants_final = [participant for sublist in participants for participant in sublist]
-   
+    participants_final = [
+        participant for sublist in participants for participant in sublist
+    ]
+
     return participants_final
 
 
 def get_entries(participants):
     entries = {k: None for k in participants}
     for person in participants:
-        layout = [[sg.Text(f"{person}'s go:"), sg.Listbox(values=["Good", "Bad"],
-                                                          default_values=None, size=(10, 10),
-                                                             key = "entry")],
-              [sg.Button("Submit")], [sg.Button("Close")]]
-        window = sg.Window(title = "Resistance", layout =layout).read()
+        layout = [
+            [
+                sg.Text(f"{person}'s go:"),
+                sg.Listbox(
+                    values=["Good", "Bad"],
+                    default_values=None,
+                    size=(10, 10),
+                    key="entry",
+                ),
+            ],
+            [sg.Button("Submit")],
+            [sg.Button("Close")],
+        ]
+        window = sg.Window(title="Resistance", layout=layout).read()
         event = window[0]
         values = window[1]
         if event == "Submit":
